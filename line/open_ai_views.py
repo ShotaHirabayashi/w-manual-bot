aa327be07896fd8069aa111b1b8b83dd2eef67fb
“ShotaHirabayashi” <bys.888812@gmail.com>
2025-03-10 12:53:39 +0900

    first commit

--- line/open_ai_views.py
+++ line/open_ai_views.py
@@ -20,7 +20,7 @@ prompt = PromptTemplate(
 質問者は疑問形で質問してこない可能性もあり、文章を汲み取って相手が知りたいことを返してください。推測での回答は避けてください。
 ドキュメントの内容はレジャーホテルのフロント業務に関するものです。できる限りドキュメントから情報を読み取り回答する意識をもってください。
 もし記載にないことが問われた場合は、「わかりません。」を伝えてください。ただしあまりにも多くの質問に「わかりません。」と答えると、ユーザーに不親切だと思われるかもしれません。
-文章は適切に返答し、適当な文章は返答しないようにしてください。
+文章は適切に返答し、適当な文章は返答しないようにしてください。チャットアプリへ返信するので、マークダウン記法ではなく、文章のみで返答してください。
 
     ドキュメント：
     {document_snippet}
@@ -38,5 +38,5 @@ def open_ai_chat(question):
     snippets = [doc.page_content for doc in document_snippet]
     snippets = "\n---\n".join(snippets)
     filled_prompt = prompt.format(document_snippet=snippets, question=question)
-    response = llm.invoke(filled_prompt)
+    response = llm.invoke(filled_prompt, max_tokens=500)
     return response.content
